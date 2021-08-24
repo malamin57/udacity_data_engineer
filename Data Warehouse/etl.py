3e8472626154028f6706cf7965e76a3fe743f3c3
Mustafa Alamin <mustafa.a.alamin@gmail.com>
2021-08-23 22:20:24 -0500

    Update etl.py

--- Data Warehouse/etl.py
+++ Data Warehouse/etl.py
@@ -4,15 +4,21 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    This function will load data from the log files into the staging table
+    """
     for query in copy_table_queries:
-    "This code load the data from the log files into the staging tables"
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    
+    This function will load take the data from the staging tables into the star schema table. 
+    
+    """
     for query in insert_table_queries:
-        "This code take the data from the staging into the tables that create the start scheme"
         cur.execute(query)
         conn.commit()
 
@@ -20,7 +26,6 @@ def insert_tables(cur, conn):
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
-    "This code reads from the config file that allows the log files into the redshift"
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
     
